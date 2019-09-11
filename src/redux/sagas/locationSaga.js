1f6f01e691518e6956e081642d781baf4657afd3
dmheisel <d.m.heisel@gmail.com>
2019-09-11 09:59:15 -0500

    add location saga and fetch location saga encapsulated in try/catch

--- src/redux/sagas/locationSaga.js
+++ src/redux/sagas/locationSaga.js
@@ -2,13 +2,28 @@ import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
 
 function* fetchLocations() {
-  //get locations from server-database connection
-  let response = yield axios.get('/api/locations');
-  yield put({type:'SET_LOCATIONS', payload: response.data})
+	//get locations from server-database connection
+	try {
+		let response = yield axios.get('/api/locations');
+		yield put({ type: 'SET_LOCATIONS', payload: response.data });
+  } catch (error) {
+    console.log('error on fetching locations and setting to redux state')
+  }
+}
+
+function* addLocation(action) {
+	//add location to database
+	try {
+    yield axios.post('/api/locations', action.payload);
+	  yield put({type: 'FETCH_LOCATIONS'});
+  } catch (error) {
+    console.log('error on saga sending location to server')
+  }
 }
 
 function* locationSaga() {
-  yield takeLatest('FETCH_LOCATIONS', fetchLocations)
+	yield takeLatest('FETCH_LOCATIONS', fetchLocations);
+	yield takeLatest('ADD_LOCATION', addLocation);
 }
 
 export default locationSaga;
