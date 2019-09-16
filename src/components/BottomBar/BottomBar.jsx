96eee2d5c2bdb006f492c288bb51876886250197
dmheisel <d.m.heisel@gmail.com>
2019-09-16 13:41:49 -0500

    back to default button connected using SET_CENTER map reducer on users default coordinates

--- src/components/BottomBar/BottomBar.jsx
+++ src/components/BottomBar/BottomBar.jsx
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import TypeMenu from '../TypeMenu/TypeMenu'
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import TypeMenu from '../TypeMenu/TypeMenu';
 import { withStyles } from '@material-ui/core/styles';
-import {
-	BottomNavigation,
-	BottomNavigationAction,
-} from '@material-ui/core';
+import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
 import AddCircleOutline from '@material-ui/icons/AddCircleOutline';
 
 const styles = theme => ({
@@ -38,15 +36,26 @@ class BottomBar extends Component {
 		return (
 			<div>
 				<BottomNavigation showLabels className={classes.root}>
-					<BottomNavigationAction
-						label='Filter Map' />
+					<BottomNavigationAction label='Filter Map' />
 					<BottomNavigationAction
 						label={this.props.addMode ? 'Cancel Add' : 'Add Site'}
 						icon={<AddCircleOutline />}
-						onClick={this.props.addMode ? this.props.toggleAddMode : this.handleOpen}
+						onClick={
+							this.props.addMode ? this.props.toggleAddMode : this.handleOpen
+						}
 					/>
 					<BottomNavigationAction
-						label='Back to Default' />
+						label='Back to Default'
+						onClick={() =>
+							this.props.dispatch({
+								type: 'SET_CENTER',
+								payload: {
+									lat: Number(this.props.user.lat),
+									lng: Number(this.props.user.lng)
+								}
+							})
+						}
+					/>
 				</BottomNavigation>
 				<TypeMenu
 					id='simple-menu'
@@ -54,12 +63,16 @@ class BottomBar extends Component {
 					keepMounted
 					open={Boolean(this.state.anchorEl)}
 					handleClose={this.handleClose}
-					handleSelect={this.handleSelect}/>
+					handleSelect={this.handleSelect}
+				/>
 			</div>
 		);
 	}
 }
 const mapStateToProps = reduxStore => ({
-	markerTypes: reduxStore.markers.markerTypes
-})
-export default connect(mapStateToProps)(withStyles(styles)(BottomBar));
+	markerTypes: reduxStore.markers.markerTypes,
+	user: reduxStore.user
+});
+export default connect(mapStateToProps)(
+	withStyles(styles)(withRouter(BottomBar))
+);
--- src/components/ConfirmationDialog/ConfirmationDialog.jsx
+++ src/components/ConfirmationDialog/ConfirmationDialog.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 //material-ui imports
 import {
 	Button,
